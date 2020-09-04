a57ae13289e1b1b40c05c60332d6c0712cf56ad3
Michael Jade Bengil <mike.bengil@gmail.com>
2020-09-04 18:25:09 +0800

    quiz

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,36 @@
-
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+module.exports = function (env) {
+  return {
+    entry: {
+        main: './src/js/index.js',      
+        vendors: ['./src/js/vendors/vendor1.js', './src/js/vendors/vendor2.js'] 
+    },
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: '[name].[contentHash].bundle.js',
+      publicPath: '/',
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js)$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+      }),
+    ],
+  };
+};
